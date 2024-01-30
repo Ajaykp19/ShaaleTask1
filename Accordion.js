2ce4bc78808fc2f051946bb1ce47b2f42d9a0389
Ajaykp19 <ajykp19.04@gmail.com>
2024-01-30 21:53:28 +0530

    committed

--- Accordion.js
+++ Accordion.js
@@ -31,35 +31,35 @@ export default function Accordion() {
         <p>In case you </p>
         <p>have questions.</p>
         </div>
-      <div class="accordion accordion-flush" id="accordionFlushExample" >
-  <div class="accordion-item my-5" style={heading} >
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed " style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      <div className="accordion accordion-flush" id="accordionFlushExample" >
+  <div className="accordion-item my-5" style={heading} >
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed " style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
         How do I subscribe?
       </button>
     </h2>
-    <div id="flush-collapseOne"  class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body" style={con}>Login or Create an account on Shaale. Head to shaale.com/subscribe or click 'Subscribe' in the footer to make a payment using your Credit card / Debit card / Netbanking / UPI and subscribe</div>
+    <div id="flush-collapseOne"  className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body" style={con}>Login or Create an account on Shaale. Head to shaale.com/subscribe or click 'Subscribe' in the footer to make a payment using your Credit card / Debit card / Netbanking / UPI and subscribe</div>
     </div>
   </div>
-  <div class="accordion-item my-5" style={heading}>
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+  <div className="accordion-item my-5" style={heading}>
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
         What are the modes of payment you accept?
       </button>
     </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body" style={con}>We accept all major Credit Cards, Debit Cards, Netbanking, Digital Wallets & UPI</div>
+    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body" style={con}>We accept all major Credit Cards, Debit Cards, Netbanking, Digital Wallets & UPI</div>
     </div>
   </div>
-  <div class="accordion-item my-5" style={heading}>
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+  <div className="accordion-item my-5" style={heading}>
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" style={heading} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
         Does my subscribe auto-renew?
       </button>
     </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body" style={con}>Yes. But you can choose to cancel your subscription anytime under shaale.com/myorders.</div>
+    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body" style={con}>Yes. But you can choose to cancel your subscription anytime under shaale.com/myorders.</div>
     </div>
   </div>
 </div>
